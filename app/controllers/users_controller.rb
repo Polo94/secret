f244b9dc36ae4072f544785f51a3948280bca06f
Polo94 <paul.henry.claudant@gmail.com>
2018-02-05 19:13:45 +0100

    login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+
+  def show
+  	@user = User.find(params[:id])
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		log_in @user
+  		flash[:success] = "Bravo"
+  		redirect_to @user
+  	else
+  		rendez 'new'
+  	end
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:email, :password_config)
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'sessions/new'
+
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#new'
+delete '/logout', to: 'sessions#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

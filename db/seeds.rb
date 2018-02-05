96cb2b8d23a5069aad3e29f85b7e1b79a552955f
Polo94 <paul.henry.claudant@gmail.com>
2018-02-06 00:31:48 +0100

    hope4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(email: 'xavierniel@thp.com')
+user = User.create(email: 'xavierniel@thp.com', password: 'thpls')
